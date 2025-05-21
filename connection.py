676e2c70877a8210aa4e9f971fab3a73b4aa66dc
mghizlat idrissi <162041720+FATIMAzahra202@users.noreply.github.com>
2025-05-21 18:40:12 +0100

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,4 +1,21 @@
 import sqlite3
 
 def connect_db():
-    return sqlite3.connect("segula_chatbot.db")
+    conn = sqlite3.connect("segula_chatbot.db")
+    create_users_table(conn)
+    return conn
+
+def create_users_table(conn):
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            role TEXT NOT NULL
+        )
+    """)
+    conn.commit()
+    cursor.close()
